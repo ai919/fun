f3b823a7c7ba7b9e11052b090b02da6425ecf605
ai717 <evergreen333@gmail.com>
2025-11-23 10:51:53 -0500

    99

--- lib/topbar.php
+++ lib/topbar.php
@@ -118,12 +118,11 @@ function render_topbar($isTestPage = false, $isHomePage = false) {
                     <span class="tub-text">退出</span>
                 </a>
             <?php else: ?>
-                <a href="/login.php" class="tub-link tub-link-icon" title="登录">
+                <a href="/login.php" class="tub-link tub-link-icon tub-link-login" title="登录">
                     <span class="tub-icon">
                         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
-                            <polyline points="10 17 15 12 10 7"></polyline>
-                            <line x1="15" y1="12" x2="3" y2="12"></line>
+                            <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                            <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
                         </svg>
                     </span>
                     <span class="tub-text">登录</span>
@@ -213,12 +212,11 @@ function render_topbar($isTestPage = false, $isHomePage = false) {
                 <span class="tub-text">退出</span>
             </a>
         <?php else: ?>
-            <a href="/login.php" class="tub-link tub-link-icon" title="登录">
+            <a href="/login.php" class="tub-link tub-link-icon tub-link-login" title="登录">
                 <span class="tub-icon">
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
-                        <polyline points="10 17 15 12 10 7"></polyline>
-                        <line x1="15" y1="12" x2="3" y2="12"></line>
+                        <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                        <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
                     </svg>
                 </span>
                 <span class="tub-text">登录</span>
