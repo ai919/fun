25b067c5a87dc8d79423c3955eccc4180cbec483
ai717 <evergreen333@gmail.com>
2025-11-23 12:36:11 -0500

    清理历史文件
    清理历史文件

--- admin/edit_test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$query = '';
-if (isset($_GET['id']) && ctype_digit((string)$_GET['id'])) {
-    $query = '?id=' . (int)$_GET['id'];
-}
-header('Location: /admin/test_edit.php' . $query);
-exit;
